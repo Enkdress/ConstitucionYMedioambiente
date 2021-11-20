a33239a30089c3ee9e963ff5f3eb4df4d6829492
Sergio Correa <felizjanuk12@gmail.com>
2021-11-20 11:31:50 -0500

    Starting the project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      {/* Banner */}
+      {/* Title */}
+      {/* Article */}
+      {/* Carousel of pictures */}
+      {/* Carousel of audios */}
+    </div>
+  );
+}
+
+export default App;
